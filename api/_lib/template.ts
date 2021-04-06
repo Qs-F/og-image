418da6cbf3e30b4ab341b7f3f798d3ca8012bd02
Qs-F <qsf@de-liker.com>
2021-04-07 01:19:26 +0900

    Revert "不必要な画像パラメータを消した"
    This reverts commit 23faafce6c897728aec376a0c1319378eea492c6.

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -103,7 +103,7 @@ function getCss(theme: string, fontSize: string) {
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize } = parsedReq;
+    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
     return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
@@ -115,6 +115,12 @@ export function getHtml(parsedReq: ParsedRequest) {
     <body>
         <div>
             <div class="spacer">
+            <div class="logo-wrapper">
+                ${images.map((img, i) =>
+                    getPlusSign(i) + getImage(img, widths[i], heights[i])
+                ).join('')}
+            </div>
+            <div class="spacer">
             <div class="heading">${emojify(
                 md ? marked(text) : sanitizeHtml(text)
             )}
@@ -124,3 +130,16 @@ export function getHtml(parsedReq: ParsedRequest) {
 </html>`;
 }
 
+function getImage(src: string, width ='auto', height = '225') {
+    return `<img
+        class="logo"
+        alt="Generated Image"
+        src="${sanitizeHtml(src)}"
+        width="${sanitizeHtml(width)}"
+        height="${sanitizeHtml(height)}"
+    />`
+}
+
+function getPlusSign(i: number) {
+    return i === 0 ? '' : '<div class="plus">+</div>';
+}
--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -7,4 +7,7 @@ export interface ParsedRequest {
     theme: Theme;
     md: boolean;
     fontSize: string;
+    images: string[];
+    widths: string[];
+    heights: string[];
 }
